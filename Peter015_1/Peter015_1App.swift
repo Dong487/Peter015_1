e4621a3e811312d208ccce85badffea607172ba3
Dong487 <Dsheng7777777@gmail.com>
2022-07-21 11:13:33 +0800

    Initial Commit

---
+++ Peter015_1/Peter015_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Peter015_1App.swift
+//  Peter015_1
+//
+//  Created by DONG SHENG on 2022/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Peter015_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Peter015_1Tests/Peter015_1Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Peter015_1Tests.swift
+//  Peter015_1Tests
+//
+//  Created by DONG SHENG on 2022/7/21.
+//
+
+import XCTest
+@testable import Peter015_1
+
+class Peter015_1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
